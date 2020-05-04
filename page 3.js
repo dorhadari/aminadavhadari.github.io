a36325e52bcd9e6d69b74988e5eee807e4a473b7
dorhadari <63163369+dorhadari@users.noreply.github.com>
2020-05-04 17:58:55 +0300

    Add files via upload

--- page 3.js
+++ page 3.js
@@ -10,13 +10,3 @@ $(document).ready(function(){
     })
 })
 
-/*$(document).ready(function(){
-    $('body').scroll(function(){
-        if ($(this).scrollTop() > 1000){
-            $(".section:nth-child(2)").css({"opacity" : "0.3"})
-        }
-        else {
-            $(".section:nth-child(2)").css({"opacity" : "1"})
-        }
-    })
-})*/
