89305aa41f9989bbeb4e895f11f5bfc267314729
dorhadari <63163369+dorhadari@users.noreply.github.com>
2020-05-30 14:17:51 +0300

    Add files via upload

--- Other.js
+++ Other.js
@@ -1,9 +1,8 @@
 class Draggable{
 
     constructor(){
-    this.selector = ".note";
-    this.handle = ".pin";
     this.onMouseMove = this.onMouseMove.bind(this);
+    this.Zindex = 1;
     }
     onMouseMove(e){
      const target = this.target;
@@ -12,34 +11,46 @@ class Draggable{
                 x: e.clientX,
                 y: e.clientY
             }
-            console.log(pose)
             target.style.top = pose.y + 'px';
             target.style.left = pose.x + 'px';
     }
-    START(){
-         Array.from(document.querySelectorAll(this.selector)).forEach(el => {
+    START(options){
+        if(!options){
+            return;
+        }
+        if(options.selector && options.handle){
+            this.selector = options.selector;
+            this.handle = options.handle;
+              Array.from(document.querySelectorAll(this.selector)).forEach(el => {
     
                 el.addEventListener('mousedown', (e) => {
-                	const target = e.target;
-                	const handle = target.closest(this.handle);
-                	if(handle){
-                		const __target = target.closest(this.selector);
-                		if(__target){
-                		this.target = __target;
-                	   window.addEventListener('mousemove', this.onMouseMove);
-                		}
-                	
-                	}
+                    const target = e.target;
+                    const handle = target.closest(this.handle);
+                    if(handle){
+                        const __target = target.closest(this.selector);
+                        if(__target){
+                        this.target = __target;
+                       window.addEventListener('mousemove', this.onMouseMove);
+                        }
+                    
+                    }
                
     
                 })
                 window.addEventListener('mouseup', (e) => {
+                    this.Zindex = this.Zindex +1;
+                    debugger
+                    this.target.style.zIndex = this.Zindex;
                     window.removeEventListener('mousemove', this.onMouseMove);
                 })
     
             })
+        }
+       
     }
 }
     
+
     const drag = new Draggable();
-    drag.START();
+    // selector => position: absolute || fixed
+    drag.START({selector:".note",handle:".pin"});
