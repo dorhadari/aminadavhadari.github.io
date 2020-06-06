4c3df6f7ce73b4f3970cf2917614633cb3d79375
dorhadari <63163369+dorhadari@users.noreply.github.com>
2020-06-06 09:36:46 +0300

    Add files via upload

--- about.js
+++ about.js
@@ -1,15 +1,7 @@
 $(window).scroll(function(){
     $(".section:nth-child(1)").css("opacity", 1 - $(window).scrollTop() / 100);
   });
-/*
-$(window).scroll(function() {
-if ($(this).scrollTop() > 100) {
-debugger 
-$('.section:nth-child(1)').css('background','#11337B');
-} else {
-$('.section:nth-child(1)').css('background', "");
-}
-});*/
+
 
 
 
@@ -42,22 +34,3 @@ $(window).scroll(function()
 onScroll();
 });
 
-/*$(document).ready(function(){
-    $('html').scroll(function(){
-        if ($(this).scrollTop() > 100){
-            $(".section:nth-child(1)").css({"opacity" : "0.3"})
-            var box5 = document.querySelector('.box5');
-            if(box5){
-            	box5.style.display = "none";
-            }
-            if ($(this).scrollTop() > 4000) {
-   			if(box5){
-            	box5.style.display = "";
-            }
-            }
-        }
-        else {
-            $(".section:nth-child(1)").css({"opacity" : "1"})
-        }
-    })
-})*/
